f4f38e0c589caaf6ac8f79aaa9b1be026362d0b5
Ashwani Yadav <yadav8800766165@gmail.com>
2025-10-23 00:18:17 +0530

    todo in react with context api

---
+++ src/components/Form.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import { useTodo } from '../context';
+
+const Form = () => {
+    const[todo,setTodo]=useState("");
+    const{addTodo}=useTodo();
+    const add=(e)=>{
+        e.preventDefault()
+
+        addTodo({todo,completed:false})
+        setTodo("")
+    }
+  return (
+    <form className='flex' onSubmit={add}>
+        <input value={todo} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder='Write todo...' 
+        className='w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5'
+        />
+        <button type='submit' className='rounded-r-lg px-3 py-1 bg-green-600 shrink-0'>Add</button>
+    </form>
+  )
+}
+
+export default Form
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import TodoItem from "./TodoItem";
+import Form from "./Form";
+
+export {TodoItem,Form}
